69388f3c5437e92b3835fc593e1b7881368c63c9
torkhani <torkhani.moetez@gmail.com>
2016-12-21 22:57:01 +0100

    add sponsorship entity

---
+++ Entity/Sponsorship.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Doctoubib\ModelsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Sponsorship
+ *
+ * @ORM\Table(name="sponsorship")
+ * @ORM\Entity(repositoryClass="Doctoubib\ModelsBundle\Repository\SponsorshipRepository")
+ */
+class Sponsorship
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="doctor_name", type="string", length=255)
+     */
+    private $doctorName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="region", type="string", length=255)
+     */
+    private $region;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return Sponsorship
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Sponsorship
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set doctorName
+     *
+     * @param string $doctorName
+     *
+     * @return Sponsorship
+     */
+    public function setDoctorName($doctorName)
+    {
+        $this->doctorName = $doctorName;
+
+        return $this;
+    }
+
+    /**
+     * Get doctorName
+     *
+     * @return string
+     */
+    public function getDoctorName()
+    {
+        return $this->doctorName;
+    }
+
+    /**
+     * Set region
+     *
+     * @param string $region
+     *
+     * @return Sponsorship
+     */
+    public function setRegion($region)
+    {
+        $this->region = $region;
+
+        return $this;
+    }
+
+    /**
+     * Get region
+     *
+     * @return string
+     */
+    public function getRegion()
+    {
+        return $this->region;
+    }
+}
+
